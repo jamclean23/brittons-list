bd64209e0ceee81ac04aa850f568ca87b2739bae
jamclean23 <jamclean23@gmail.com>
2024-04-16 15:54:27 -0400

    Initial Commit

---
+++ readme.md
@@ -0,0 +1,4 @@
+<h1>Overview</h1>
+<p>
+A template for creating apps with Webpack and React.
+</p>
---
+++ src/React/App.js
@@ -0,0 +1,129 @@
+// Main app
+
+
+// ====== IMPORTS ======
+
+// React
+import React from 'react';
+import { Component }  from './components/Component.js';
+
+
+// ====== FUNCTIONS ======
+
+// Main app function 
+function App () {
+    return (
+        <>
+            <header>
+                <h1>Britton's Picks!</h1>
+            </header>
+
+            <main>
+            <ul>
+                Seen:
+                <li>A.I.C.O. Incarnation</li>
+                <li>Ajin Demi-Human</li>
+                <li>Aldnoah.Zero</li>
+                <li>Altered Carbon: Resleeved</li>
+                <li>Arifureta: From Commonplace to World's Strongest</li>
+                <li>Attack on Titan</li>
+                <li>Arcane</li>
+                <li>Avatar (original and legend of Korra)</li>
+                <li>Berserk (all)</li>
+                <li>Black Lagoon</li>
+                <li>BLAME!</li>
+                <li>Blue-eye Samurai</li>
+                <li>BNA</li>
+                <li>Castlevania</li>
+                <li>Canon Busters</li>
+                <li>Captain Harlock: Space Pirate</li>
+                <li>Cells at Work</li>
+                <li>Children of the Whale</li>
+                <li>Cowboy Bebop</li>
+                <li>Cyber Punk: Edgerunners</li>
+                <li>Death Note</li>
+                <li>Delicious in Dungeon</li>
+                <li>Demonslayer</li>
+                <li>Devilman Crybaby</li>
+                <li>Discovery - Daft Punk</li>
+                <li>DOTA Dragons Blood</li>
+                <li>Dragon Pilot</li>
+                <li>Dragons Dogma</li>
+                <li>Dragonball Z (OG stuff)</li>
+                <li>Elfen Lied</li>
+                <li>Erased</li>
+                <li>Expelled from Paradise</li>
+                <li>Fairy Tale</li>
+                <li>Fate/Stay Series</li>
+                <li>Final Fantasy (Advent Children and Spirits Within)</li>
+                <li>Frieren: Beyond Journey's End</li>
+                <li>Full Metal Alchemist</li>
+                <li>Gantz:0</li>
+                <li>Ghost in the Shell (movies and anime)</li>
+                <li>Gundam Wing: Iron Blooded Children</li>
+                <li>Gurren Lagann</li>
+                <li>Halo Legends</li>
+                <li>Hunter x Hunter</li>
+                <li>Inuyasha</li>
+                <li>Is it wrong to pick up girls in a dungeon?</li>
+                <li>Japan Sinks 2020</li>
+                <li>Kabaneri of the Iron Fortress</li>
+                <li>Kill la Kill</li>
+                <li>Log Horizon</li>
+                <li>Love, Death + Robots</li>
+                <li>Lupin III: The Castle of Cagliostro</li>
+                <li>Made in Abyss</li>
+                <li>Mash Le</li>
+                <li>Memories (1995)</li>
+                <li>Mushii Shi</li>
+                <li>Neon Genesis Evangelion, The End of Evangelion, Evangelion Death (True)2</li>
+                <li>One Punch Man</li>
+                <li>Orbital Children</li>
+                <li>Our Last Crusade or the Rise of a New World</li>
+                <li>Overlord</li>
+                <li>Paprika</li>
+                <li>Parasyte: Maxim</li>
+                <li>Pokemon (OG stuff)</li>
+                <li>Sailor Moon (old stuff)</li>
+                <li>Scavenger's Reign</li>
+                <li>Seven Deadly Sins</li>
+                <li>Sonny Boy</li>
+                <li>Star Wars the Clone Wars</li>
+                <li>Steins;Gate</li>
+                <li>Steins;Gate 0</li>
+                <li>Sturgill Simpson Presents Sound and Fury</li>
+                <li>Sword Art Online (all lol)</li>
+                <li>Toradora</li>
+                <li>Trigun (old and new)</li>
+                <li>Vinland Saga</li>
+                <li>Violet Evergarden</li>
+                <li>Yasuke</li>
+                <li>Your Lie in April</li>
+                <li>Your Name</li>
+                <li>Yuri!!! On Ice</li>
+            </ul>
+
+
+            <ul>
+                To Watch:
+                <li>Akira</li>
+                <li>Perfect Blue</li>
+                <li>Clannad?</li>
+                <li>FLCL</li>
+                <li>Samurai Champloo</li>
+                <li>Goblin Slayer</li>
+                <li>Space Battle Yamato</li>
+                <li>Vampire hunter D</li>
+            </ul>
+
+            </main>
+
+        </>
+    );
+}
+
+// ====== EXPORTS ======
+
+export {
+    App
+}
